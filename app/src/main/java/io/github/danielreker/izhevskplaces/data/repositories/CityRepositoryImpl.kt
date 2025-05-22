315ddcd3cff1eed812e26fbe20eb7b3f21fa36d6
DanielReker <sasha29072004@gmail.com>
2025-05-22 12:39:23 +0400

    refactor: optimize flows

--- app/src/main/java/io/github/danielreker/izhevskplaces/data/repositories/CityRepositoryImpl.kt
+++ app/src/main/java/io/github/danielreker/izhevskplaces/data/repositories/CityRepositoryImpl.kt
@@ -6,8 +6,7 @@ import io.github.danielreker.izhevskplaces.model.Category
 import io.github.danielreker.izhevskplaces.model.Recommendation
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOn
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.flowOf
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -17,23 +16,17 @@ class CityRepositoryImpl @Inject constructor(
     private val cityProvider: CityProvider,
 ) : CityRepository {
 
-    override suspend fun getCity(): Flow<City> = flow {
-        emit(cityProvider.getCity())
-    }.flowOn(Dispatchers.IO)
+    override suspend fun getCity(): Flow<City> = flowOf(cityProvider.getCity())
 
-    override suspend fun getCategories(): Flow<List<Category>> = flow {
-        emit(cityProvider.getAllCategories())
-    }.flowOn(Dispatchers.IO)
+    override suspend fun getCategories(): Flow<List<Category>> =
+        flowOf(cityProvider.getAllCategories())
 
-    override suspend fun getCategory(categoryId: String): Flow<Category?> = flow {
-        emit(cityProvider.getCategoryById(categoryId))
-    }.flowOn(Dispatchers.IO)
+    override suspend fun getCategory(categoryId: String): Flow<Category?> =
+        flowOf(cityProvider.getCategoryById(categoryId))
 
-    override suspend fun getRecommendationsForCategory(categoryId: String): Flow<List<Recommendation>> = flow {
-        emit(cityProvider.getRecommendationsByCategory(categoryId))
-    }.flowOn(Dispatchers.IO)
+    override suspend fun getRecommendationsForCategory(categoryId: String): Flow<List<Recommendation>> =
+        flowOf(cityProvider.getRecommendationsByCategory(categoryId))
 
-    override suspend fun getRecommendation(recommendationId: String): Flow<Recommendation?> = flow {
-        emit(cityProvider.getRecommendationById(recommendationId))
-    }.flowOn(Dispatchers.IO)
+    override suspend fun getRecommendation(recommendationId: String): Flow<Recommendation?> =
+        flowOf(cityProvider.getRecommendationById(recommendationId))
 }
